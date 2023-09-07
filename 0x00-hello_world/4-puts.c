9b1d8bd8a8fcf9b5f120d2af0c8b58456f09dc5d
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-09-07 12:28:47 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Calls puts and recieves value returned by puts after its
+ * execution.
+ * Return: Always return 0 to indicate success
+ */
+ 
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
