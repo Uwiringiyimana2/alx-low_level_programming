250542095ba083360c7fef3efde9d60611e00efe
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-09-12 10:56:28 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+/*
+ * file: 0-putchar.c
+ * Auth: U Eric
+ */
+#include "main.h"
+
+/*
+ * main: a program that prints _putchar
+ * return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
