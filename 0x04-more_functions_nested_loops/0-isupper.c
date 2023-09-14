df0803ea2de6772ac1ffbfa756dbd8b268d59c73
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-09-14 06:10:26 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * isupper - checks for uppercase character
+ * @c: the character to be checked 
+ * Return: 1 if c is uppercase and 0 if lowercase
+ */
+
+int _isupper(int c)
+{	
+	return (c >= 'A' && c <= 'Z');
+{
