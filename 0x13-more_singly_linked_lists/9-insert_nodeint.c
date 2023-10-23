e422bf78255079a1a53580cffe3606d2c1a6fd7f
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-10-23 21:42:29 +0200

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,25 +6,25 @@
  * @n: value of the inserted node
  * Return: pointer to head of list
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *current, *new;
 
 	current = *head;
 	new = malloc(sizeof(listint_t));
-	if (index == NULL)
+	if (idx == NULL)
 		return (NULL);
 	new->n = n;
-	if (index == 0)
+	if (idx == 0)
 	{
 		new->next = current;
 		*head = new;
 		return (*head);
 	}
-	while (index > 1)
+	while (idx > 1)
 	{
 		current = current->next;
-		index--;
+		idx--;
 		if (!current)
 		{
 			free(new);
