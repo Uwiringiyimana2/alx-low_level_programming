472a6bd8cb84010a8c514ce2503c907f5aa2f907
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-10-26 20:39:17 +0200

    bit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be converted
+ */
+void print_binary(unsigned long int n)
+{
+	char b;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	print_binary(n >> 1);
+	b = (n & 1) ? '1' : '0';
+	_putchar(b);
+}
