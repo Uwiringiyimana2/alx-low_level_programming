d048ef5c7f16269d5bc6fa2e3a1a2d54691a984b
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-10-30 21:01:16 +0200

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	input = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
+	input = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 
 	if (input == -1)
 		return (-1);
