aa490246266dfa28ca09ec6789eda24aef183893
Eric Uwiringiyimana <uwiringiyimanaericmax2000@gmail.com>
2023-12-07 14:27:50 +0200

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	i = 0;
 	current = head;
-	while (i < index)
+	while (current != NULL && i < index)
 	{
 		current = current->next;
 		i++;
